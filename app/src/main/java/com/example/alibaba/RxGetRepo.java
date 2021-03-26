63882b2881d135401314fb4c272881ed52c192b9
armanziaei <armanziaei83@gmaio.com>
2021-03-26 19:48:05 +0430

    Feat RxGetRepo Class : RxJava performed on GetRepo model for retrofit process

---
+++ app/src/main/java/com/example/alibaba/RxGetRepo.java
@@ -0,0 +1,19 @@
+package com.example.alibaba;
+
+import com.example.alibaba.Models.GetRepo;
+import com.example.alibaba.Retrofit.RetrofitHolder;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class RxGetRepo {
+
+    public Observable<GetRepo> getRepoObservable(String owner , String repo){
+
+        return new RetrofitHolder().getApi()
+                .getRepsDetail(owner , repo)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+}
