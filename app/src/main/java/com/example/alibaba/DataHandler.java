564733750461dbcb6f5d1c4397aff2c706b3fa2f
armanziaei <armanziaei83@gmaio.com>
2021-03-26 20:27:14 +0430

    Feat DataHandler Class : this class created for deleting and adding data to Room data base

---
+++ app/src/main/java/com/example/alibaba/DataHandler.java
@@ -0,0 +1,90 @@
+package com.example.alibaba;
+
+import android.content.Context;
+import android.provider.ContactsContract;
+
+import androidx.room.Room;
+
+import com.example.alibaba.DataBase.DataBase;
+import com.example.alibaba.DataBase.RepoEntity;
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+
+import io.reactivex.Completable;
+import io.reactivex.CompletableObserver;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.schedulers.Schedulers;
+
+public class DataHandler {
+
+    Context context;
+    DataBase dataBase;
+
+    public void createDataBase(Context context){
+
+        this.context = context;
+        dataBase = Room.databaseBuilder(context , DataBase.class , "repoinfo.db" ).build();
+    }
+
+    public void addToDb(int id , int forks , int stars , String desc , String collaborators , String userOwner,String url ){
+        Completable.fromAction(new Action() {
+            @Override
+            public void run() throws Exception {
+                dataBase.repoDao().insertData(new RepoEntity(id , forks , stars , desc , collaborators , userOwner , url));
+            }
+        }).observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new CompletableObserver() {
+                    @Override
+                    public void onSubscribe(@NotNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        makeSout("Added");
+                    }
+
+                    @Override
+                    public void onError(@NotNull Throwable e) {
+                        System.out.println("Error : " + e.getMessage());
+                    }
+                });
+    }
+
+    public void deleteData(int id){
+
+        Completable.fromAction(new Action() {
+            @Override
+            public void run() throws Exception {
+
+                dataBase.repoDao().deleteDataById(id);
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new CompletableObserver() {
+                    @Override
+                    public void onSubscribe(@NotNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        makeSout("Deleting Process Finished");
+                    }
+
+                    @Override
+                    public void onError(@NotNull Throwable e) {
+
+                    }
+                });
+    }
+
+    private void makeSout(String message) {
+        System.out.println(message);
+    }
+}
